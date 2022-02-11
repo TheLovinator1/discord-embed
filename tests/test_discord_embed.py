170896af6e720db51d1a35f07fc9b3dfd72f0364
Joakim Hellsén <tlovinator@gmail.com>
2022-02-12 00:31:26 +0100

    Add test for checking if domain ends with slash

--- tests/test_discord_embed.py
+++ tests/test_discord_embed.py
@@ -1,5 +1,10 @@
 from discord_embed import __version__
+from discord_embed.settings import Settings
 
 
 def test_version():
     assert __version__ == "0.1.0"
+
+
+def test_domain_ends_with_slash():
+    assert not Settings.domain.endswith("/")
