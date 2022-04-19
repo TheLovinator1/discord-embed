fec6ccec0a328c10a24134679e8c96410a911e2c
Joakim Hellsén <tlovinator@gmail.com>
2022-04-19 16:39:07 +0200

    Add docstrings to tests

--- tests/test_discord_embed.py
+++ tests/test_discord_embed.py
@@ -1,10 +1,11 @@
-from discord_embed import __version__
-from discord_embed.settings import Settings
+from discord_embed import __version__, settings
 
 
 def test_version():
+    """Test that version is correct."""
     assert __version__ == "0.1.0"
 
 
 def test_domain_ends_with_slash():
-    assert not Settings.domain.endswith("/")
+    """Test that domain ends with slash."""
+    assert not settings.domain.endswith("/")
